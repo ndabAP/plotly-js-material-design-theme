90a1b605ce73221466777d0b90dd9a4f4f00ecec
s8wecy <kontakt@julian-claus.de>
2018-04-04 08:55:44 +0200

    feat: improve scatter, add bar

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 export const scatter = {
-  line: {
-    width: 2
+  lines: {
+    line: {
+      width: 2
+    }
   },
 
-  marker: {
-    size: 1
-  }
+  markers: {}
 }
 
+export const bar = {}
+
 export const layout = {
-  colorway: ['#3F51B5', '#E91E63', '#9C27B0', '#673AB7', '#F44336', '#2196F3', '#03A9F4', '#00BCD4', '#009688'],
+  font: {
+    family: 'Roboto, sans-serif'
+  },
+
+  colorway: ['#4285f4', '#3F51B5', '#E91E63', '#9C27B0', '#673AB7', '#F44336', '#2196F3', '#03A9F4', '#00BCD4', '#009688'],
 
   xaxis: {
+    gridcolor: '#E0E0E0',
+
     titlefont: {
-      family: 'Roboto, sans-serif'
+      color: '#424242',
+      size: 12
     },
 
     tickfont: {
-      family: 'Roboto, sans-serif',
       color: '#757575',
       size: 12
     },
@@ -26,12 +34,14 @@ export const layout = {
   },
 
   yaxis: {
+    gridcolor: '#E0E0E0',
+
     titlefont: {
-      family: 'Roboto, sans-serif'
+      color: '#424242',
+      size: 12
     },
 
     tickfont: {
-      family: 'Roboto, sans-serif',
       color: '#757575',
       size: 12
     }
--- test/src/index.js
+++ test/src/index.js
@@ -1,14 +1,42 @@
 import Plotly from 'plotly.js'
-import {layout, scatter} from '../../src'
+import {layout, scatter, bar} from '../../src'
 
-// Scatter
-const trace = {
+const configuartion = {displayModeBar: false}
+
+// Type: Scatter
+
+// Mode: Lines
+const scatterlines = {
   x: [1, 2, 3, 4],
   y: [10, 15, 13, 17],
   type: 'scatter',
-  ...scatter
+  mode: 'lines',
+  ...scatter.lines
 }
 
-const data = [trace]
+layout.xaxis.title = 'Title'
+layout.yaxis.title = 'Title'
 
-Plotly.newPlot('scatter', data, layout)
+Plotly.newPlot('scatter-lines', [scatterlines], layout, configuartion)
+
+// Mode: Lines
+const scattermarkers = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  type: 'scatter',
+  mode: 'markers',
+  ...scatter.markers
+}
+
+Plotly.newPlot('scatter-markers', [scattermarkers], layout, configuartion)
+
+// Type: Bar
+
+const bardefault = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  type: 'bar',
+  ...bar
+}
+
+Plotly.newPlot('bar', [bardefault], layout, configuartion)
