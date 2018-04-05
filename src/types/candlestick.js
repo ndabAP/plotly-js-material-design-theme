1f5f7e14f7205e20bab7380baa8f3fa9012940f6
s8wecy <kontakt@julian-claus.de>
2018-04-05 15:03:11 +0200

    feat: add candlestick, ohlc

---
+++ src/types/candlestick.js
@@ -0,0 +1,19 @@
+export const candlestick = {
+  decreasing: {
+    line: {
+      color: '#3F51B5',
+      width: 1
+    },
+
+    fillcolor: '#3F51B5'
+  },
+
+  increasing: {
+    line: {
+      color: '#4285F4',
+      width: 1
+    },
+
+    fillcolor: '#4285F4'
+  }
+}
