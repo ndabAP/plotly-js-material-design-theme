ad367e4807a4a643d9c6895281e3689e8927f86e
s8wecy <kontakt@julian-claus.de>
2018-04-06 12:19:40 +0200

    fix(pie): increase hole, add correct colors

--- src/types/pie.js
+++ src/types/pie.js
@@ -1,6 +1,7 @@
 export const pie = {
-  hole: 0.5,
-  textfont: {
-    color: '#fff'
+  hole: 0.75,
+  textposition: 'none',
+  marker: {
+    colors: ['#4285f4', '#45a5f5', '#93d5ed', '#93d5ed']
   }
 }
