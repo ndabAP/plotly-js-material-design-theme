86b15d578febab1c1790a95a0757e240ae6118a0
rymw3p <kontakt@julian-claus.de>
2018-04-08 09:18:39 +0200

    fix(ohlc): add correct colors

--- src/types/ohlc.js
+++ src/types/ohlc.js
@@ -1,3 +1,15 @@
 export const ohlc = {
+  decreasing: {
+    line: {
+      color: '#3F51B5',
+      width: 1
+    }
+  },
 
+  increasing: {
+    line: {
+      color: '#3F51B5',
+      width: 1
+    }
+  }
 }
