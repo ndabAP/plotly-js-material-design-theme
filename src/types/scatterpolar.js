57049697828bbf2526b6ecee8867782642bac093
s8wecy <kontakt@julian-claus.de>
2018-04-22 09:50:59 +0200

    feat(scatterpolar): add fill color

--- src/types/scatterpolar.js
+++ src/types/scatterpolar.js
@@ -3,5 +3,7 @@ import defaultsDeep from 'lodash.defaultsdeep'
 export const scatterpolar = configuration => defaultsDeep(configuration, {
   line: {
     width: 2
-  }
+  },
+
+  fillcolor: 'rgba(171, 71, 188, 0.3)'
 })
