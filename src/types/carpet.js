5e1a4b83854aa09405e4f0beeb74aa61f25c4e26
s8wecy <kontakt@julian-claus.de>
2018-09-24 16:25:15 +0200

    fix(carpet): increase line width

--- src/types/carpet.js
+++ src/types/carpet.js
@@ -2,10 +2,12 @@ import defaultsDeep from 'lodash.defaultsdeep'
 
 export const carpet = configuration => defaultsDeep(configuration, {
   aaxis: {
-    color: '#4285F4'
+    color: '#4285F4',
+    gridwidth: 2
   },
 
   baxis: {
-    color: '#4285F4'
+    color: '#4285F4',
+    gridwidth: 2
   }
 })
